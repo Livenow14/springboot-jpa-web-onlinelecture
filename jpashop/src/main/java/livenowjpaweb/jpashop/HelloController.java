ae9c45373e94c1b580381bcb4c599419f8efb805
Livenow14 <xowjd41@naver.com>
2020-03-31 15:30:33 +0900

    Tyhmeleaf가 자동적으로 경로와 확장자를 설정하는것을 확인.

--- jpashop/src/main/java/livenowjpaweb/jpashop/HelloController.java
+++ jpashop/src/main/java/livenowjpaweb/jpashop/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data","하하잇!");
-        return "hello";
+        return "hello"; //이렇게 해도 Thymeleaf 가 자동적으로 template 의 hello.html를 찾아붐
+                        //앞의 경로와 뒤의 확장자를 자동으로 설정.
     }
 }
