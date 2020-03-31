904ed18b91fdc46f7681bbd33a619ef59b954a72
Livenow14 <xowjd41@naver.com>
2020-03-31 15:28:57 +0900

    View 환경 설정_ Devtools 이용법과 Tyhmeleaf 이용법을 배움.

--- jpashop/src/main/java/livenowjpaweb/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/livenowjpaweb/jpashop/JpashopApplication.java
@@ -5,13 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpashopApplication {
-
 	public static void main(String[] args) {
-
-		Hello hello= new Hello();
-		hello.setDate("hello");
-		String Data = hello.getDate();
-		System.out.println("Data = " + Data);
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 	}
