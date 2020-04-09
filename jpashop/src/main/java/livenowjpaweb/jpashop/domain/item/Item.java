8fed114d60e216d5d4fdec472b43f77889880ff5
Livenow14 <xowjd41@naver.com>
2020-04-09 16:28:46 +0900

    상품 엔티티(비즈니스 노릭 추가)& 상품 리포지토리 개발 EntityManager에 대한 이해와 em.createQuery 에 대한 이해 필요

--- jpashop/src/main/java/livenowjpaweb/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/livenowjpaweb/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package livenowjpaweb.jpashop.domain.item;
 
 
 import livenowjpaweb.jpashop.domain.Category;
+import livenowjpaweb.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,5 +28,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")         //다대다 예제를 위해, 실무에선 거의 안쓴다
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직 == //
+
+    //stock 증가
+    public void addStock(int quntity){
+        this.stockQuantity+=quntity;
+    }
+    // item entity에 stockQuantity가 있으니 여기서 관리하는게 좋음
+    //stock 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+
+    }
 
 }
