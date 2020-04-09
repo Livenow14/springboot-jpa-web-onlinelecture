ed0a8ec9f4771fb3fc1dae6a6cbaf58020e2a8e7
Livenow14 <xowjd41@naver.com>
2020-04-09 16:46:16 +0900

    상품 서비스 개발_ Repository 부분 다시볼것

---
+++ jpashop/src/main/java/livenowjpaweb/jpashop/service/ItemService.java
@@ -0,0 +1,33 @@
+package livenowjpaweb.jpashop.service;
+
+import livenowjpaweb.jpashop.domain.item.Item;
+import livenowjpaweb.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional // readonly가 true이기때문
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id){
+        return itemRepository.findOne(id);
+    }
+
+
+
+}
