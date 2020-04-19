f4d2a7f15b5a05bdb83d08d09e7e56e349bf169d
Livenow14 <xowjd41@naver.com>
2020-04-20 00:33:53 +0900

    웹 계층 개발_ 상품 등록_ moder.addAttribute("", new ~ ) ""부분의 글자와 createItemForm.html에서 가져오는 글자가 같아야 한다.

---
+++ jpashop/src/main/java/livenowjpaweb/jpashop/controller/ItemController.java
@@ -0,0 +1,38 @@
+package livenowjpaweb.jpashop.controller;
+
+
+import livenowjpaweb.jpashop.domain.item.Book;
+import livenowjpaweb.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createFrom(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+
+        Book book = new Book();                         //이것은 이후에 바꿔야한다. set을 이렇게 하는건 좋지않다.
+        book.setName(form.getName());                   //실무에서는 setter를 다 랄림
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
