51a6fc02e578798b19b64ba6e6d03f46d6587c7b
Livenow14 <xowjd41@naver.com>
2020-04-21 23:31:56 +0900

    웹 계층 개발_ 변경 감지와 병합(merge), merge는 전체를 한번에 수정하기 때문에 실무에서 실 사용을 하지않는다.

--- jpashop/src/main/java/livenowjpaweb/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/livenowjpaweb/jpashop/controller/ItemController.java
@@ -50,7 +50,7 @@ public class ItemController {
         return "items/itemList";
     }
 
-    @GetMapping("items/{itemId}/edit")  //{itemId}는 path variable
+    @GetMapping("/items/{itemId}/edit")  //{itemId}는 path variable
     public String updateItemFrom(@PathVariable("itemId") Long itemId, Model model){
         Book item = (Book) itemService.findOne(itemId);         //이렇게 쓰는건 좋지않다. 예제를 위한 예제이다 .캐스팅 하는건 왠만하면 지양
 
@@ -66,18 +66,24 @@ public class ItemController {
         return "items/updateItemForm";
     }
 
-    @PostMapping("items/{itemId}/edit")  //{itemId}는 path variable
-    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookForm form){             //html에서 잡은 form이라는 attribute를 사용하기위해 @ModelAttribute사용
-                                          ///alt+ shift+ insert를 하면 column select 모드가 됨
-        Book book = new Book();           //shift+ tap 하면 옆으로 땡겨짐
+    @PostMapping("/items/{itemId}/edit")  //{itemId}는 path variable
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form){             //html에서 잡은 form이라는 attribute를 사용하기위해 @ModelAttribute사용
+
+       /**
+       * 준영속 엔티티?: 영속성 켄텍스가 더는 관리하지 않는 엔티티, 이미 DB에 한번 저장되어서 식별자가 존재하는것
+       * 지금은 book이 준영속 엔티티다다.
+       * 문제는_ JPA가 관리를 안한다. 왜냐면 new로 해서 만들었기때문에,
+      */                                  ///alt+ shift+ insert를 하면 column select 모드가 됨
+/*        Book book = new Book();           //shift+ tap 하면 옆으로 땡겨짐
         book.setId(form.getId());             //)ctrl+ shift+ u 하면 upper case 됨
         book.setName(form.getName());
         book.setPrice(form.getPrice());
         book.setStockQuantity(form.getStockQuantity());
         book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
-                                                //여기서 id값을 건드는건 진짜 조심해야한다. 
-        itemService.saveItem(book);
+        book.setIsbn(form.getIsbn());*/          //이렇게 어설프게 엔티티를 설정 하지 말자.
+                                                //여기서 id값을 건드는건 진짜 조심해야한다.
+
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 }
